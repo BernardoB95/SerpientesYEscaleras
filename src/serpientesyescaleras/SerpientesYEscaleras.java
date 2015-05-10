b3fb052906570b331e89308b3631706f0be94d12
BernardoB95 <bernardo.bello95@hotmail.com>
2015-05-09 21:21:20 -0430

    Elimine la prueba de matrices

--- src/serpientesyescaleras/SerpientesYEscaleras.java
+++ src/serpientesyescaleras/SerpientesYEscaleras.java
@@ -7,22 +7,8 @@ public class SerpientesYEscaleras {
     
     public static void main(String[] args) {
        
-        int cont=1;
         
-        for (int i = 0; i < 8; i++) 
-            for (int j = 0; j < 8; j++) 
-                for (int k = 0; k < 8; k++) 
-                {
-                    System.out.println("");
-                    for (int l = 8; l > 0; l--) 
-                    {
-                        System.out.print(cont+"\t");
-                        cont++;
-                    }
-                }
-            
         
-            
         
     }
     
