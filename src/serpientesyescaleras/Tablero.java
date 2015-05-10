caa76ed0e2c67b04579b51c1aaec0782b3edb63d
BernardoB95 <bernardo.bello95@hotmail.com>
2015-05-09 21:54:30 -0430

    Metodos CREAR e IMPRIMIR matriz
    Los dos metodos estan en la clase tablero, tiene pase por parametros
    para que lean a la matriz

--- src/serpientesyescaleras/Tablero.java
+++ src/serpientesyescaleras/Tablero.java
@@ -9,7 +9,36 @@ public class Tablero {
     private Fichas Matriz[][] = new Fichas[8][8];
     
     
+    public void CrearTableroJuego(Fichas[][] Matriz)//Metodo de creacion de tablero
+    {
+        int cont=0;
+        for (int i = 7; i >= 0; i--) //<== Algoritmo de Guille
+        {
+            if (i%2==0 || i==0) 
+            {
+                for (int j = 7; j >= 0; j--) 
+                {
+                    System.out.print("\t"+Matriz[i][j]);                   
+                }
+            }
+            else
+            {
+                for (int j = 0; j < 8; j++) 
+                {
+                    System.out.print("\t"+Matriz[i][j]);     
+                }
+            }
+            
+        }
+    }
     
+    public void MostrarTablero(Fichas[][] Matriz)//Impresion de tablero, parametro MATRIZ
+    {
+        System.out.println("            TABLERO");
+        CrearTableroJuego(Matriz);
+        
+        
+    }
     
     
 }
